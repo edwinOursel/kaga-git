30e2078258aa166fabbb6977c75921c3d84da4c9
Edwin <edwin.oursel@codeworks.fr>
2025-02-13 22:08:52 +0100

    réparé le +

--- lib/bisect.dart
+++ lib/bisect.dart
@@ -1,6 +1,7 @@
 il manquait un import
 c'est le début d'une grande aventure !!!
 et une nouvelle classe
-les fractales, c'est le nec plus ultra
+les fractales, c'est le nec + ultra
+mais les ensemble de cantor, c'est la mort
 on rajoute des bouts
 
