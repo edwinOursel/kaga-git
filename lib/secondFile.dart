c9f48497af2901b3a9f6455a72c35d9483c4ad52
Edwin <edwin.oursel@codeworks.fr>
2025-02-14 08:48:46 +0100

    éternalisation de second feature

--- lib/secondFile.dart
+++ lib/secondFile.dart
@@ -1,1 +1,4 @@
 I needed to write a new feature after de first
+
+argh, je croyais que ça allait être plus simple et prendre moins de temps
+j'aurais dû revenir sur main, la review de first feature traîne depuis longtemps...
